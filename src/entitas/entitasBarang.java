c0636eac7092041e6bfe7428ebfea6f519d0c3ca
11140360 <>
2016-11-09 06:23:32 -0800

    Pembuatan desain form barang

---
+++ src/entitas/entitasBarang.java
@@ -0,0 +1,81 @@
+
+package entitas;
+
+public class entitasBarang {
+    private String kode,nama,jenis;
+    private int jumlah;
+    private float hargaJual,hargaBeli;
+    
+    public entitasBarang(){}
+
+    /**
+     * @return the kode
+     */
+    public String getKode() {
+        return kode;
+    }
+
+    /**
+     * @param kode the kode to set
+     */
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the jenis
+     */
+    public String getJenis() {
+        return jenis;
+    }
+
+    /**
+     * @param jenis the jenis to set
+     */
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    /**
+     * @return the jumlah
+     */
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    /**
+     * @param jumlah the jumlah to set
+     */
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    /**
+     * @return the hargaJual
+     */
+    public float getHargaJual() {
+        return hargaJual;
+    }
+
+    /**
+     * @param hargaJual the hargaJual to set
+     */
+    public void setHargaJual(float hargaJual) {
+        this.hargaJual = hargaJual;
+    }
+   
+}
